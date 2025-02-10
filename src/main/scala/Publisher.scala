41deb2e42cae969e4b8fa9fb78adc0cbd5325ad2
asela-maduwantha <maduwanthaaselagra@gmail.com>
2025-02-10 08:59:19 +0530

    change var to val

--- src/main/scala/Publisher.scala
+++ src/main/scala/Publisher.scala
@@ -11,34 +11,27 @@ object Publisher {
     factory.setUsername("admin")
     factory.setPassword("Dilshan@123")
 
-    var connection: com.rabbitmq.client.Connection = null
-    var channel: com.rabbitmq.client.Channel = null
 
-    try {
-      connection = factory.newConnection()
-      channel = connection.createChannel()
+      val connection = factory.newConnection()
+      val channel = connection.createChannel()
 
       channel.queueDeclare(QUEUE_NAME, false, false, false, null)
 
       var message: String = ""
 
-      println("Type message that you want to send and type 'done' when you need to exit")
+      println("Type message that you want to send and type 'x' when you need to exit")
 
-      // Adding a delay to ensure the consumer is ready
       Thread.sleep(1000)
 
-      while (message != "done") {
+      while (message != "x") {
         message = StdIn.readLine("Enter Message to send: ")
         if (message != "done") {
           channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"))
           println("Sent to Queue")
         }
       }
-    } catch {
-      case ex: Exception => ex.printStackTrace()
-    } finally {
       if (channel != null) channel.close()
       if (connection != null) connection.close()
-    }
+
   }
 }
